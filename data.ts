d29dd7b107f412c6b1d6e5f221322ae463723a47
mohazard555 <mohazardmoha272@gmail.com>
2025-10-23 13:02:30 +0300

    feat: Add initial video data and improved channel description
    Populates the `INITIAL_VIDEOS` array with several videos from the "Masha and the Bear" series, enhancing the initial content of the Jana Kids Video Hub. Also, the `INITIAL_CHANNEL_DESCRIPTION` has been expanded to provide a more detailed and engaging overview of the channel's offerings in Arabic.

--- data.ts
+++ data.ts
@@ -1,13 +1,420 @@
 import type { Video, Playlist, Activity } from './types';
 
-export const INITIAL_CHANNEL_DESCRIPTION: string = 'استمتع بأجمل القصص التعليمية والمسلية من قناة Jana Kids';
+export const INITIAL_CHANNEL_DESCRIPTION: string = "قناة مخصصة للأطفال تقدم مجموعة متنوعة من قصص الأطفال الممتعة والهادفة..\nتتميز القناة بتقديم محتوى غني بالفيديوهات التي تحفز خيال الأطفال وتعلمهم القيم والأخلاق من خلال حكايات مسلية..\nيتم سرد القصص بأسلوب مبسط وجذاب..\n مع الرسوم المتحركة أو الصور الملونة التي تساعد الأطفال على التفاعل مع القصة وفهمها بشكل أفضل..\nيمكن أن تتضمن القناة أيضًا قصصًا تربوية تركز على تعزيز مفاهيم مثل الصدق والصداقة والشجاعة..\nبالإضافة إلى قصص خيالية تثير الفضول وتمنحهم المتعة والتسلية..";
 
-export const INITIAL_VIDEOS: Video[] = [];
+export const INITIAL_VIDEOS: Video[] = [
+  {
+    "id": 1761213316760,
+    "title": "ماشا والدب في حديقة الحيوانات",
+    "youtubeUrl": "https://www.youtube.com/watch?v=jdBckerjw_o",
+    "thumbnailUrl": "https://img.youtube.com/vi/jdBckerjw_o/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213300034,
+    "title": "ماشا والدب في مزرعة الابقار ",
+    "youtubeUrl": "https://www.youtube.com/watch?v=rlaFBGbb9bM&t=43s",
+    "thumbnailUrl": "https://img.youtube.com/vi/rlaFBGbb9bM/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213286927,
+    "title": "ماشا والدب في العطلة الصيفية",
+    "youtubeUrl": "https://www.youtube.com/watch?v=erkoPYDJrnw",
+    "thumbnailUrl": "https://img.youtube.com/vi/erkoPYDJrnw/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213271097,
+    "title": "ماشا والدب في مدينة الالعاب",
+    "youtubeUrl": "https://www.youtube.com/watch?v=eSxvDU6Mf-0",
+    "thumbnailUrl": "https://img.youtube.com/vi/eSxvDU6Mf-0/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213256321,
+    "title": "ماشا والدب على سطح القمر",
+    "youtubeUrl": "https://www.youtube.com/watch?v=ojkGb3viXME",
+    "thumbnailUrl": "https://img.youtube.com/vi/ojkGb3viXME/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213241568,
+    "title": "النمر والارنب",
+    "youtubeUrl": "https://www.youtube.com/watch?v=4SI9OoriZ5Y&t=49s",
+    "thumbnailUrl": "https://img.youtube.com/vi/4SI9OoriZ5Y/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213224875,
+    "title": "الارنب السريع والسلحفاه",
+    "youtubeUrl": "https://www.youtube.com/watch?v=8-v5WMeYtrQ&pp=0gcJCQYKAYcqIYzv",
+    "thumbnailUrl": "https://img.youtube.com/vi/8-v5WMeYtrQ/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213210362,
+    "title": "معركة الاسد والارنب",
+    "youtubeUrl": "https://www.youtube.com/watch?v=QvorxrT8NlY",
+    "thumbnailUrl": "https://img.youtube.com/vi/QvorxrT8NlY/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213194427,
+    "title": "الاغنام السبعة",
+    "youtubeUrl": "https://www.youtube.com/watch?v=FcmwprZc8N0",
+    "thumbnailUrl": "https://img.youtube.com/vi/FcmwprZc8N0/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213158498,
+    "title": "ابطال الغابة الاسد والقرد",
+    "youtubeUrl": "https://www.youtube.com/watch?v=wm9mFcnKd7I&pp=0gcJCQYKAYcqIYzv",
+    "thumbnailUrl": "https://img.youtube.com/vi/wm9mFcnKd7I/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213141512,
+    "title": "الاسد بائع الايس كريم",
+    "youtubeUrl": "https://www.youtube.com/watch?v=BGamU0Z4Cos",
+    "thumbnailUrl": "https://img.youtube.com/vi/BGamU0Z4Cos/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213132080,
+    "title": "السنافروالاسد",
+    "youtubeUrl": "https://www.youtube.com/watch?v=4Z2HcY0gv5w&pp=0gcJCQYKAYcqIYzv",
+    "thumbnailUrl": "https://img.youtube.com/vi/4Z2HcY0gv5w/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213114826,
+    "title": "السنافر والقطط",
+    "youtubeUrl": "https://www.youtube.com/watch?v=Uap8aB1Svr4",
+    "thumbnailUrl": "https://img.youtube.com/vi/Uap8aB1Svr4/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213098087,
+    "title": "مغامرة الماعز مع الدجاجة",
+    "youtubeUrl": "https://www.youtube.com/watch?v=zXLKvl7YO_k",
+    "thumbnailUrl": "https://img.youtube.com/vi/zXLKvl7YO_k/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213082284,
+    "title": "سنفور والعصفور",
+    "youtubeUrl": "https://www.youtube.com/watch?v=H2FBGAA0exE&t=50s",
+    "thumbnailUrl": "https://img.youtube.com/vi/H2FBGAA0exE/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213063720,
+    "title": "السنجاب والسنافر",
+    "youtubeUrl": "https://www.youtube.com/watch?v=8128ledsXqk",
+    "thumbnailUrl": "https://img.youtube.com/vi/8128ledsXqk/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213039704,
+    "title": "الفأر الطماع",
+    "youtubeUrl": "https://www.youtube.com/watch?v=Qa-fWJwU7Mg",
+    "thumbnailUrl": "https://img.youtube.com/vi/Qa-fWJwU7Mg/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213029329,
+    "title": "قصص تعليمية ممتعة",
+    "youtubeUrl": "https://www.youtube.com/watch?v=KJI4vfvtADQ",
+    "thumbnailUrl": "https://img.youtube.com/vi/KJI4vfvtADQ/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761213019188,
+    "title": "النملة والاسد",
+    "youtubeUrl": "https://www.youtube.com/watch?v=hv4Sn_eCw7Q&t=11s",
+    "thumbnailUrl": "https://img.youtube.com/vi/hv4Sn_eCw7Q/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761212906159,
+    "title": "سنفور والنملة",
+    "youtubeUrl": "https://www.youtube.com/watch?v=Gz5104vezyU&t=17s",
+    "thumbnailUrl": "https://img.youtube.com/vi/Gz5104vezyU/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761212867114,
+    "title": "الارنب والاسد في البئر العجيب",
+    "youtubeUrl": "https://www.youtube.com/watch?v=dTxOKAyS1C4",
+    "thumbnailUrl": "https://img.youtube.com/vi/dTxOKAyS1C4/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761212847515,
+    "title": "الارنب والاسد في بحيرة الاسماك",
+    "youtubeUrl": "https://www.youtube.com/watch?v=DPXCbH5XUD8",
+    "thumbnailUrl": "https://img.youtube.com/vi/DPXCbH5XUD8/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761212526760,
+    "title": "الارنب والاسد رحلة الى الفضاء",
+    "youtubeUrl": "https://www.youtube.com/watch?v=tw0PvmajvJI",
+    "thumbnailUrl": "https://img.youtube.com/vi/tw0PvmajvJI/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761212513194,
+    "title": "الارنب والاسد في الجزيرة العجيبة",
+    "youtubeUrl": "https://www.youtube.com/watch?v=qL3eAiBd7W4",
+    "thumbnailUrl": "https://img.youtube.com/vi/qL3eAiBd7W4/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761212499233,
+    "title": "الارنب والاسد مع البطة",
+    "youtubeUrl": "https://www.youtube.com/watch?v=bhq3pAP6FwM",
+    "thumbnailUrl": "https://img.youtube.com/vi/bhq3pAP6FwM/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761212472016,
+    "title": "تعلم الاحرف العربية للاطفال",
+    "youtubeUrl": "https://www.youtube.com/watch?v=0fvS0ZvRSH4",
+    "thumbnailUrl": "https://img.youtube.com/vi/0fvS0ZvRSH4/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761212457801,
+    "title": "القطة مربية الطفل",
+    "youtubeUrl": "https://www.youtube.com/watch?v=M3T_E14ftTg",
+    "thumbnailUrl": "https://img.youtube.com/vi/M3T_E14ftTg/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761212445736,
+    "title": "مغامرات القط والطفل",
+    "youtubeUrl": "https://www.youtube.com/watch?v=69z2A1r_wZk",
+    "thumbnailUrl": "https://img.youtube.com/vi/69z2A1r_wZk/sddefault.jpg",
+    "views": 1
+  },
+  {
+    "id": 1761212425947,
+    "title": "الاغنام السبعة",
+    "youtubeUrl": "https://www.youtube.com/watch?v=SIYeW-0N0Ws&pp=0gcJCQYKAYcqIYzv",
+    "thumbnailUrl": "https://img.youtube.com/vi/SIYeW-0N0Ws/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761212401552,
+    "title": "الكب الغبي",
+    "youtubeUrl": "https://www.youtube.com/watch?v=zW0_ba9Lg7A",
+    "thumbnailUrl": "https://img.youtube.com/vi/zW0_ba9Lg7A/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761212381690,
+    "title": "الغراب الاسود العجيب",
+    "youtubeUrl": "https://www.youtube.com/watch?v=1X7tXO_8Iqw",
+    "thumbnailUrl": "https://img.youtube.com/vi/1X7tXO_8Iqw/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761212365865,
+    "title": "تويتي المشاغب",
+    "youtubeUrl": "https://www.youtube.com/watch?v=zU8Zs2wsQrw",
+    "thumbnailUrl": "https://img.youtube.com/vi/zU8Zs2wsQrw/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761211989377,
+    "title": "فأر المطبخ الصغير",
+    "youtubeUrl": "https://www.youtube.com/watch?v=yhMRgWQEooQ",
+    "thumbnailUrl": "https://img.youtube.com/vi/yhMRgWQEooQ/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761211955043,
+    "title": "دبدوب الاخضر والبط",
+    "youtubeUrl": "https://www.youtube.com/watch?v=bSRfkCRwqNo",
+    "thumbnailUrl": "https://img.youtube.com/vi/bSRfkCRwqNo/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761211943016,
+    "title": "مدرسة الحيوانات المضحكون",
+    "youtubeUrl": "https://www.youtube.com/watch?v=4dfEz2t9RaI",
+    "thumbnailUrl": "https://img.youtube.com/vi/4dfEz2t9RaI/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761211926128,
+    "title": "ماذا حصل على السفينة",
+    "youtubeUrl": "https://www.youtube.com/watch?v=2xFWC_8CvwU",
+    "thumbnailUrl": "https://img.youtube.com/vi/2xFWC_8CvwU/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761211914195,
+    "title": "مجموعة من قصص الاسد",
+    "youtubeUrl": "https://www.youtube.com/watch?v=BiH0c-fHLlk",
+    "thumbnailUrl": "https://img.youtube.com/vi/BiH0c-fHLlk/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761211898209,
+    "title": "ماشا والدب على سفينة تايتنك",
+    "youtubeUrl": "https://www.youtube.com/watch?v=OOyKdiQyBpQ",
+    "thumbnailUrl": "https://img.youtube.com/vi/OOyKdiQyBpQ/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761211883125,
+    "title": "ماشا والدب في عالم ماريو",
+    "youtubeUrl": "https://www.youtube.com/watch?v=8nJvcRqerYM",
+    "thumbnailUrl": "https://img.youtube.com/vi/8nJvcRqerYM/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761211869654,
+    "title": "ارنوب يتعلم الحساب",
+    "youtubeUrl": "https://www.youtube.com/watch?v=czgNMwzjAKI",
+    "thumbnailUrl": "https://img.youtube.com/vi/czgNMwzjAKI/sddefault.jpg",
+    "views": 0
+  },
+  {
+    "id": 1761211778096,
+    "title": "مجموعة من قصص الاسد",
+    "youtubeUrl": "https://www.youtube.com/watch?v=vKZhHOGbp3g&t=4s",
+    "thumbnailUrl": "https://img.youtube.com/vi/vKZhHOGbp3g/sddefault.jpg",
+    "views": 0
+  }
+];
 
 export const INITIAL_SHORTS: Video[] = [];
 
 export const INITIAL_ACTIVITIES: Activity[] = [];
 
-export const INITIAL_PLAYLISTS: Playlist[] = [];
+export const INITIAL_PLAYLISTS: Playlist[] = [
+  {
+    "id": 1761211806811,
+    "name": "قصص الاسد",
+    "videoIds": [
+      1761211778096,
+      1761213210362,
+      1761213158498,
+      1761213141512,
+      1761213019188,
+      1761212867114,
+      1761212847515,
+      1761212526760,
+      1761212513194,
+      1761212499233,
+      1761211914195
+    ]
+  },
+  {
+    "id": 1761211833593,
+    "name": "قصص الارنب",
+    "videoIds": [
+      1761213241568,
+      1761213224875,
+      1761211869654
+    ]
+  },
+  {
+    "id": 1761211840850,
+    "name": "قصص القط والفأر",
+    "videoIds": [
+      1761213039704,
+      1761212457801,
+      1761212445736,
+      1761211989377
+    ]
+  },
+  {
+    "id": 1761211853721,
+    "name": "قصص ماشا والدب",
+    "videoIds": [
+      1761213316760,
+      1761213300034,
+      1761213286927,
+      1761213271097,
+      1761213256321,
+      1761211898209,
+      1761211883125
+    ]
+  },
+  {
+    "id": 1761211960834,
+    "name": "قصص الدب",
+    "videoIds": [
+      1761211955043
+    ]
+  },
+  {
+    "id": 1761212342881,
+    "name": "قصص الطيور",
+    "videoIds": [
+      1761212365865
+    ]
+  },
+  {
+    "id": 1761212347188,
+    "name": "قصص منوعة",
+    "videoIds": [
+      1761212381690,
+      1761211943016,
+      1761211926128
+    ]
+  },
+  {
+    "id": 1761212392922,
+    "name": "قصص الكلب",
+    "videoIds": [
+      1761212401552
+    ]
+  },
+  {
+    "id": 1761212431071,
+    "name": "قصص الاغنام",
+    "videoIds": [
+      1761213194427,
+      1761213098087,
+      1761212425947
+    ]
+  },
+  {
+    "id": 1761212477059,
+    "name": "قصص تعليمية",
+    "videoIds": [
+      1761213029329,
+      1761212472016
+    ]
+  },
+  {
+    "id": 1761213003957,
+    "name": "قصص السنافر",
+    "videoIds": [
+      1761213132080,
+      1761213114826,
+      1761213082284,
+      1761212906159
+    ]
+  },
+  {
+    "id": 1761213069280,
+    "name": "قصص السناجب",
+    "videoIds": [
+      1761213063720
+    ]
+  },
+  {
+    "id": 1761213229872,
+    "name": "قصص السلحفاة",
+    "videoIds": []
+  }
+];
 
-export const INITIAL_CREDENTIALS = { username: 'admin', password: 'password' };
+export const INITIAL_CREDENTIALS = { username: "admin", password: "password" };
